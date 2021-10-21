1cc509b6b7d94087f231bb2b7a6e34238ad85612
frank-cywong <mr.frankwong2005+irl@gmail.com>
2021-10-21 14:30:45 -0400

    Removes error checks

--- 01-Object/Kitty.java
+++ 01-Object/Kitty.java
@@ -2,12 +2,8 @@ public class Kitty{
 	private String _name = "Mittens";
 	private int _age = 2;
 	public Kitty(String name, int age){
-		if(age >= 0){
-			_age = age;
-		}
-		if(!name.equals(" ")){
-			_name = name;
-		}
+		_age = age;
+		name = name;
 	}
 	public Kitty(){
 	}
@@ -18,9 +14,7 @@ public class Kitty{
 		return _age;
 	}
 	public void changeName(String name){
-		if(!name.equals(" ")){
-			_name = name;
-		}
+		_name = name;
 	}
 	public void makeOlder(){
 		_age++;
