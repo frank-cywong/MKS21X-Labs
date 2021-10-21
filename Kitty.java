3f547221ddf74e1b5e6d905871c04aa389d50aab
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-10-21 13:39:51 -0400

    Add constructors

---
+++ Kitty.java
@@ -0,0 +1,14 @@
+public class Kitty{
+	private String _name = "Mittens";
+	private int _age = 2;
+	public Kitty(String name, int age){
+		if(age >= 0){
+			_age = age;
+		}
+		if(!name.equals(" ")){
+			_name = name;
+		}
+	}
+	public Kitty(){
+	}
+}
