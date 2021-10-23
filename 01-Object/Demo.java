9e82799d6231fd5439eae1ce54140f1de1efe764
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-10-22 22:42:51 -0400

    Tidys up code a bit

--- 01-Object/Demo.java
+++ 01-Object/Demo.java
@@ -10,16 +10,16 @@ public class Demo{
 	Point P4 = new Point(9,17);
 	
 	// more test cases
-	System.out.println(  distance(P1,P3)); // should be 5
-    System.out.println(  Point.distance(P1,P3)); 
-    System.out.println(  P1.distanceTo(P3));
-	System.out.println(  distance(P3,P4)); // should be 13
-    System.out.println(  Point.distance(P3,P4)); 
-    System.out.println(  P3.distanceTo(P4));
+	System.out.println(distance(P1,P3)); // should be 5
+    System.out.println(Point.distance(P1,P3)); 
+    System.out.println(P1.distanceTo(P3));
+	System.out.println(distance(P3,P4)); // should be 13
+    System.out.println(Point.distance(P3,P4)); 
+    System.out.println(P3.distanceTo(P4));
 	
-    System.out.println(  distance(P1,P2)); //call1
-    System.out.println(  Point.distance(P1,P2)); //call 2
-    System.out.println(  P1.distanceTo(P2)); //call 3
+    System.out.println(distance(P1,P2)); //call 1 - should be sqrt(2)
+    System.out.println(Point.distance(P1,P2)); //call 2
+    System.out.println(P1.distanceTo(P2)); //call 3
     //This one is IMPLIED strongly. You cannot tell by the calling style alone,
     //but you can make an educated guess.
 	
@@ -30,4 +30,4 @@ public class Demo{
 	Call 3 is non-static and in class Point
 	*/
   }
- }
+}
--- 01-Object/Point.java
+++ 01-Object/Point.java
@@ -15,9 +15,9 @@ public class Point{
     return y;
   }
   public static double distance(Point P1, Point P2){
-	  return(Math.sqrt(Math.pow(P2.x-P1.x,2)+Math.pow(P2.y-P1.y,2)));
+	return(Math.sqrt(Math.pow(P2.x-P1.x,2)+Math.pow(P2.y-P1.y,2)));
   }
   public double distanceTo(Point P){
-	  return(Math.sqrt(Math.pow(P.x-x,2)+Math.pow(P.y-y,2)));
+	return(Math.sqrt(Math.pow(P.x-x,2)+Math.pow(P.y-y,2)));
   }
 }
