a0d8d3c1149dcc4fca8c8e25e647d188db682ac7
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-10-22 22:24:50 -0400

    Add call 1 and test cases

--- 01-Object/Demo.java
+++ 01-Object/Demo.java
@@ -1,10 +1,22 @@
 public class Demo{
   /* more methods can go here*/
-
+	public static double distance(Point a, Point b){
+		return(Math.sqrt(Math.pow(b.getX()-a.getX(),2)+Math.pow(b.getY()-a.getY(),2)));
+	}
   public static void main(String[]args){
-    Point P1 = new Point(1,1);
+	Point P1 = new Point(1,1);
     Point P2 = new Point(2,2);
-
+	Point P3 = new Point(4,5);
+	Point P4 = new Point(9,17);
+	
+	// more test cases
+	System.out.println(  distance(P1,P3)); // should be 5
+    System.out.println(  Point.distance(P1,P3)); 
+    System.out.println(  P1.distanceTo(P3));
+	System.out.println(  distance(P3,P4)); // should be 13
+    System.out.println(  Point.distance(P3,P4)); 
+    System.out.println(  P3.distanceTo(P4));
+	
     System.out.println(  distance(P1,P2)); //call1
     System.out.println(  Point.distance(P1,P2)); //call 2
     System.out.println(  P1.distanceTo(P2)); //call 3
