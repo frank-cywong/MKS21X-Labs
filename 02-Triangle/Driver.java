c79ab7936b9bfa2aa146e5c77de50ffd79cf6f67
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-10-25 14:33:03 -0400

    Add Point, Triangle, and Driver

---
+++ 02-Triangle/Driver.java
@@ -0,0 +1,6 @@
+public class Driver{
+  public static void main(String[] args){
+    Point p1 = new Point(1.0, -4.12);
+    System.out.println(p1);
+  }
+}
---
+++ 02-Triangle/Point.java
@@ -0,0 +1,26 @@
+public class Point{
+  private double x,y;
+  public Point(double X, double Y){
+    x=X;
+    y=Y;
+  }
+  public Point(Point p){
+    x= p.x;
+    y= p.y;
+  }
+  public double getX(){
+    return x;
+  }
+  public double getY(){
+    return y;
+  }
+  public static double distance(Point P1, Point P2){
+	   return(Math.sqrt(Math.pow(P2.x-P1.x,2)+Math.pow(P2.y-P1.y,2)));
+  }
+  public double distanceTo(Point P){
+	   return(Math.sqrt(Math.pow(P.x-x,2)+Math.pow(P.y-y,2)));
+  }
+  public String toString(){
+    return "("+x+", "+y+")";
+  }
+}
