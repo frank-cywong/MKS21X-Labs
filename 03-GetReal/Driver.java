874251e2e9a4ac2bb5cdc04b0030049321d793e5
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-11-01 12:36:21 -0400

    Add basic rational test cases

--- 03-GetReal/Driver.java
+++ 03-GetReal/Driver.java
@@ -6,5 +6,11 @@ public class Driver{
 		System.out.println(r1.subtract(r2));
 		System.out.println(r1.multiply(r2));
 		System.out.println(r1.divide(r2)); // should be 5/32 or 0.15625
+		System.out.println(r1.getValue());
+		System.out.println(r1);
+		RationalNumber q1 = new RationalNumber(2,3);
+		RationalNumber q2 = new RationalNumber(74,28);
+		System.out.println(q1);
+		System.out.println(q2);
 	}
 }
