fdb80b7b6feb5e17b807d5d19b2943e3067cc474
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-11-22 14:35:24 -0500

    Finishes part 2

--- 06-Advent/Triangles.java
+++ 06-Advent/Triangles.java
@@ -6,11 +6,7 @@ public class Triangles {
 		int max = Math.max(a,Math.max(b,c));
 		return((a+b+c)>(max*2));
 	}
-	public static void main(String[] args){
-		String filename = "Triangle.in";
-		if(args.length > 0){ // for testing purposes
-			filename = args[0];
-		}
+	public static void method1(String filename){
 		int output = 0;
 		int temp1, temp2, temp3;
 		try{
@@ -24,10 +20,40 @@ public class Triangles {
 					output++;
 				}
 			}
-			System.out.println(output);
+			System.out.println("Part 1: "+output);
 			input.close();
 		} catch (FileNotFoundException e){
 			System.out.println("File "+filename+" not found");
 		}
+	}
+	public static void method2(String filename){
+		int output = 0;
+		int[] temp = new int[9];
+		try{
+			File f = new File(filename);
+			Scanner input = new Scanner(f);
+			while(input.hasNextInt()){
+				for(int i = 0 ; i < 9; i++){
+					temp[i] = input.nextInt();
+				}
+				for(int n = 0; n < 3; n++){
+					if(isTriangle(temp[n],temp[n+3],temp[n+6])){
+						output++;
+					} 
+				}
+			}
+			System.out.println("Part 2: "+output);
+			input.close();
+		} catch (FileNotFoundException e){
+			System.out.println("File "+filename+" not found");
+		}
+	}
+	public static void main(String[] args){
+		String filename = "Triangle.in";
+		if(args.length > 0){ // for testing purposes
+			filename = args[0];
+		}
+		method1(filename);
+		method2(filename);
 	}
 }
