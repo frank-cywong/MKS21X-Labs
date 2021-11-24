bd9c7aaa1bf025dccd12e5f8897c8be5a026ee95
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-11-24 11:16:34 -0500

    Add ribbon code

--- 06-Advent/Wrapper.java
+++ 06-Advent/Wrapper.java
@@ -1,12 +1,15 @@
 import java.io.*;
 import java.util.Scanner;
-public class Wrapping{
+public class Wrapper{
 	public static int surfacearea(int a, int b, int c){
 		int sa, sb, sc;
 		sa = a * b;
 		sb = a * c;
 		sc = b * c;
 		return(Math.min(sa,Math.min(sb,sc)) + (2 * (sa + sb + sc)));
+	}
+	public static int ribbon(int a, int b, int c){
+		return(a*b*c+2*Math.min(Math.min(a+b,b+c),a+c));
 	}
 	public static void main(String[] args) {
 		try{
@@ -16,7 +19,11 @@ public class Wrapping{
 			String[] temp;
 			while(i.hasNextLine()){
 				temp = i.nextLine().split("x");
-				output += surfacearea(Integer.parseInt(temp[0]),Integer.parseInt(temp[1]),Integer.parseInt(temp[2]));
+				if(args[1].equals("1")){
+					output += surfacearea(Integer.parseInt(temp[0]),Integer.parseInt(temp[1]),Integer.parseInt(temp[2]));
+				} else if(args[1].equals("2")){
+					output += ribbon(Integer.parseInt(temp[0]),Integer.parseInt(temp[1]),Integer.parseInt(temp[2]));
+				}
 			}
 			System.out.println(output);
 		} catch (FileNotFoundException e){
