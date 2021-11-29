134602724dfb4810883d8576eedd6d9100c3b15e
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-11-29 14:36:12 -0500

    BinarySearch and Driver class base

--- 07-BinarySearch/BinarySearch.java
+++ 07-BinarySearch/BinarySearch.java
@@ -1,5 +1,19 @@
 public class BinarySearch{
   public static int binarySearch(int[] data, int target){
-    
+    int l = 0;
+    int r = data.length - 1;
+    int m;
+    while(l <= r){
+      m = (l + r) / 2;
+      if(data[m] == target){
+        return m;
+      }
+      if(data[m] < target){
+        l = m + 1;
+      } else {
+        r = m - 1;
+      }
+    }
+    return -1;
   }
 }
