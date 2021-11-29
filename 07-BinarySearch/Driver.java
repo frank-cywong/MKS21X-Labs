64d23281bfefe22bc2f5412dd7cc8035e947c885
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-11-29 14:49:11 -0500

    Loop-based test case

--- 07-BinarySearch/Driver.java
+++ 07-BinarySearch/Driver.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Driver{
   public static void main(String[] args){
     System.out.println(BinarySearch.binarySearch(new int[]{-1,-1,-1,-1,-1,-1,-1},-1)); // should be 3
@@ -6,5 +7,23 @@ public class Driver{
     System.out.println(BinarySearch.binarySearch(new int[]{-1,-1,-1,0,1,1,1},0)); // should be 3
     System.out.println(BinarySearch.binarySearch(new int[]{-1,-1,-1,0,1,1,1},1)); // should be 5
     System.out.println(BinarySearch.binarySearch(new int[]{-1,-1,-1,0,1,1,1},-2)); // should be -1
+    int[] temp;
+    int ans;
+    for(int i = 0; i < 10; i++){
+      temp = new int[i];
+      for(int j = 0; j < i; j++){
+        temp[j] = j;
+      }
+      for(int j = -1; j < 11; j++){
+        ans = BinarySearch.binarySearch(temp,j);
+        if(ans == -1 && (j < 0 || j >= i)){
+          continue; // correct
+        }
+        if(ans == j && (j >= 0 && j < i)){
+          continue; // correct
+        }
+        System.out.println("Error in case: "+Arrays.toString(temp)+", j = "+j);
+      }
+    }
   }
 }
