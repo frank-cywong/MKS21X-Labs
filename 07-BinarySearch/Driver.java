960307c6615e13b6bb49e75d348fec687aa9e0d8
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-11-29 14:45:04 -0500

    Basic Test Cases

--- 07-BinarySearch/Driver.java
+++ 07-BinarySearch/Driver.java
@@ -1,5 +1,10 @@
 public class Driver{
   public static void main(String[] args){
-    
+    System.out.println(BinarySearch.binarySearch(new int[]{-1,-1,-1,-1,-1,-1,-1},-1)); // should be 3
+    System.out.println(BinarySearch.binarySearch(new int[]{-1,-1,-1,-1,-1,-1,-1},0)); // should be -1
+    System.out.println(BinarySearch.binarySearch(new int[]{-1,-1,-1,0,1,1,1},-1)); // should be 1
+    System.out.println(BinarySearch.binarySearch(new int[]{-1,-1,-1,0,1,1,1},0)); // should be 3
+    System.out.println(BinarySearch.binarySearch(new int[]{-1,-1,-1,0,1,1,1},1)); // should be 5
+    System.out.println(BinarySearch.binarySearch(new int[]{-1,-1,-1,0,1,1,1},-2)); // should be -1
   }
 }
