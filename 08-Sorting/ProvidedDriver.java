50ca72eb48eba13893786bba1c4d34a85126328c
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-12-06 14:41:43 -0500

    Add insertion sort support for ProvidedDriver

--- 08-Sorting/ProvidedDriver.java
+++ 08-Sorting/ProvidedDriver.java
@@ -16,13 +16,18 @@ import java.util.Arrays;
         //if it works this is for timing purposes.
         Sorts.bubbleSort(randish);
       }
+      if(artie[1].equals("insertion")){
+        Sorts.insertionSort(randish);
+      }
       if(artie[1].equals("test")){
         //if you need to check if it works this is for verification of correctness
         int[] randish2 = Arrays.copyOf(randish,randish.length);
         int[] randish3 = Arrays.copyOf(randish,randish.length);
+        int[] randish4 = Arrays.copyOf(randish,randish.length);
         Sorts.selectionSort(randish);
         Sorts.bubbleSort(randish2);
         Arrays.sort(randish3);
+        Sorts.insertionSort(randish4);
         if( Arrays.equals(randish,randish3)){
           System.out.println("Selection Correct!");
         }else{
@@ -32,6 +37,11 @@ import java.util.Arrays;
           System.out.println("Bubble Correct!");
         }else{
          System.out.println("Bubble BROKEN!!!!");
+        }
+        if( Arrays.equals(randish4,randish3)){
+          System.out.println("Insertion Correct!");
+        }else{
+         System.out.println("Insertion BROKEN!!!!");
         }
       }
     }
