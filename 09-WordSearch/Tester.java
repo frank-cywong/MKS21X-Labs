80c108e170ee1786db80809a5c5149dc48903699
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-12-09 14:39:51 -0500

    More test cases

--- 09-WordSearch/Tester.java
+++ 09-WordSearch/Tester.java
@@ -10,6 +10,18 @@ public class Tester{
     System.out.println(b.addWordHorizontal("zrq",3,0));
     System.out.println(b);
   }
+  public static void basicTest2(WordSearch b){
+    System.out.println(b.addWordDiagonal("test",3,3));
+    System.out.println(b.addWordDiagonal(b.reverse("test"),0,0));
+    System.out.println(b.addWordHorizontal("test",3,3));
+    System.out.println(b.addWordHorizontal(b.reverse("test"),3,0));
+    System.out.println(b.addWordVertical("test",3,3));
+    System.out.println(b.addWordVertical(b.reverse("test"),0,3));
+    System.out.println(b.addWordHorizontal("testing",0,0));
+    System.out.println(b.addWordVertical("inteermediate",0,4));
+    System.out.println(b.addWordHorizontal("atest",6,2));
+    System.out.println(b);
+  }
   public static void main(String[] args){
     WordSearch b = new WordSearch(0,0);
     basicTest(b);
@@ -25,5 +37,11 @@ public class Tester{
     basicTest(b); // true, false, false
     b = new WordSearch(5,7);
     basicTest(b); // true, false, true, true, true, false, true
+    b = new WordSearch(13,7);
+    basicTest2(b);
+    b = new WordSearch(4,7);
+    basicTest2(b);
+    b = new WordSearch(4,4);
+    basicTest2(b);
   }
 }
