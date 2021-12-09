f339f21e1b1b4b9f58175fbd6d20afb834b3ed0a
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-12-09 18:20:25 -0500

    Add [untested] addWord()

--- 09-WordSearch/WordSearch.java
+++ 09-WordSearch/WordSearch.java
@@ -134,19 +134,33 @@ public class WordSearch{
     }
 
     /**Attempts to add a given word in an arbitrary direction.
-     *Direction is determined using a number from 0 to 7 (inclusive), with 0 referring to the string being added
-     *from the bottom to top, with the provided position being the bottom, and 1-7 being represented by the tip
-     *of the word being rotated 45*(direction) degrees such that, for example, 2 being equal to addWordHorizontal,
-     *3 being equal to addWordDiagonal, 4 being equal to addWordVertical, and 6 referring to adding the word from
-     *back to front towards the left.
+     *Direction is determined using colinc and rolinc which represent the distance between two consecutive characters. If both rowinc and colinc are 0, the word will not be inserted.
      *@param word is the word to be added to the puzzle.
      *@param row is the row to start adding the word to.
      *@param col is the column to start adding the word to.
-     *@param dir is the direction to add the word in relative to the start point, as specified in the description above.
-     *@return true if the word was added successfully, false if it wasn't added successfully and the board wasn't modified.
+     *@param rowinc is the horizontal increment for each character, which can be an integer between -1 and 1.
+     *@param colinc is the vertical increment for each character, which can be an integer between -1 and 1.
+     *@return true if the word was added successfully, false if it wasn't added successfully, or if both rowinc and colinc are 0, and the board wasn't modified.
      */
-    public boolean addWord(String word, int row, int col, int dir){
-      
+    public boolean addWord(String word, int row, int col, int rowinc, int colinc){
+      if(rowinc == 0 && colinc == 0){ // invalid rowinc-colinc combination
+        return false;
+      }
+      if(row >= data.length || col >= data[row].length){ // invalid row/col values
+        return false;
+      }
+      if(((row + word.length() * rowinc) > data.length) || ((col + word.length() * colinc) > data[row].length)){ // out of bounds
+        return false;
+      }
+      for(int i = 0; i < word.length(); i++){
+        if(data[row+i*rowinc][col+i*colinc] != '_' && data[row+i*rowinc][col+i*colinc] != word.charAt(i)){ //overlapping word that is invalid
+          return false;
+        }
+      }
+      for(int i = 0; i < word.length(); i++){ // actually fill in the word
+        data[row+i*rowinc][col+i*colinc] = word.charAt(i);
+      }
+      return true;
     }
 
     /**Reverses a string character by character.
