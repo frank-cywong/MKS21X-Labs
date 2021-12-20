8d781ceeb900e111c5409dea99542eeee7895b7e
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-12-20 14:42:49 -0500

    Add more test cases

--- 11-ExtendArrayList/NoNullTester.java
+++ 11-ExtendArrayList/NoNullTester.java
@@ -26,8 +26,25 @@ public class NoNullTester{
     System.out.println(n);
     OrderedArrayList<String> o = new OrderedArrayList<String>();
     o.add("test1");
-    o.add("test2");
+    o.add(0,"test2");
     o.set(1,"test0");
+    o.add("test2");
+    o.add(0,"test1.5");
+    o.set(2,"test3");
+    o.set(3,"test1.3");
+    try{
+      o.add(null);
+      System.out.println("SHOULD NOT OCCUR: NO ERROR CALLED ON ADDING NULL");
+    } catch(IllegalArgumentException e){
+      System.out.println("Successfully failed to add null");
+    }
+    System.out.println(o);
+    try{
+      o.set(2,null);
+      System.out.println("SHOULD NOT OCCUR: NO ERROR CALLED ON TRYING TO SET NULL");
+    } catch(IllegalArgumentException e){
+      System.out.println("Successfully failed to set value to null");
+    }
     System.out.println(o);
   }
 }
