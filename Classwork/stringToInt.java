2679cbf621bccb94e57a73463c478414f0e7843c
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2021-12-21 14:50:36 -0500

    Make stringToInt() work for negative numbers

--- Classwork/stringToInt.java
+++ Classwork/stringToInt.java
@@ -4,13 +4,23 @@ public class stringToInt{
   }
   public static int stringToInt(String s){
     int o = 0;
+    boolean sign = false; // - or +
+    if(s.charAt(0) == '-'){
+      sign = true;
+      s = s.substring(1);
+    }
     for(int i = 0; i < s.length(); i++){
       o *= 10;
-      o += valueOfDigit(s.charAt(i));
+      if(sign){
+        o -= valueOfDigit(s.charAt(i));
+      } else {
+        o += valueOfDigit(s.charAt(i));
+      }
     }
     return o;
   }
   public static void main(String[] args){
     System.out.println(stringToInt("84172301"));
+    System.out.println(stringToInt("-1294318"));
   }
 }
