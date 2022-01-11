c5c947368f27f2348983cde17a525df78f17404f
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2022-01-11 14:49:43 -0500

    Make color character printing tester

--- 12-Stuyablo/Screen.java
+++ 12-Stuyablo/Screen.java
@@ -5,13 +5,13 @@ public class Screen{
     for(int i = 0; i < w; i++){
       ws += borderchar;
     }
-    for(int i = 0; i < h; i++){
-      if(i == 0 || i == (h-1)){
+    for(int i = 1; i <= h; i++){
+      if(i == 1 || i == h){
         System.out.println(Text.colorize(ws, Text.BACKGROUND + Text.CYAN));
       }
       else {
         System.out.print(Text.colorize(borderchar, Text.BACKGROUND + Text.CYAN));
-        Text.go(i + 1, w);
+        Text.go(i, w);
         System.out.println(Text.colorize(borderchar, Text.BACKGROUND + Text.CYAN));
       }
     }
@@ -20,5 +20,24 @@ public class Screen{
     Text.clear();
     Text.go(1,1);
     border(80,30);
+    int[] temp = new int[4];
+    for(int i = 0; i < temp.length; i++){
+      temp[i] = (int)(Math.random() * 100);
+    }
+    int sc = 2;
+    int inc = 78 / 4;
+    for(int i = 0; i < temp.length; i++){
+      int val = temp[i];
+      Text.go(2, sc);
+      if(val < 25){
+        System.out.print(Text.colorize(""+val, Text.RED));
+      } else if(val > 75){
+        System.out.print(Text.colorize(""+val, Text.GREEN));
+      } else {
+        System.out.print(Text.colorize(""+val, Text.WHITE));
+      }
+      sc += inc;
+    }
+    Text.go(31,1);
   }
 }
