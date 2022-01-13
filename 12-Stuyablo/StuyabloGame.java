f9c06ffbc63987303f657682d5bc6c392962cd7a
frank-cywong <90653148+frank-cywong@users.noreply.github.com>
2022-01-13 14:50:53 -0500

    Ports border and screen drawing from Game.java, adds drawText and starts drawParty

--- 12-Stuyablo/StuyabloGame.java
+++ 12-Stuyablo/StuyabloGame.java
@@ -3,10 +3,46 @@ public class StuyabloGame{
   private static final int WIDTH = 80;
   private static final int HEIGHT = 30;
   private static final int BORDER_COLOR = Text.BLACK;
-  private static final int BORDER_BACKGROUND = Text.WHITE + Text.BACKGROUND;
+  private static final int BACKGROUND_COLOR = Text.CYAN + Text.BACKGROUND;
+  private static final String HORIZONTALCHAR = "-";
+  private static final String VERTICALCHAR = "|";
+  private static final String CORNERCHAR = "O";
+  private static final int DEFAULT_TEXT_COLOR = Text.WHITE;
 
   public static void main(String[] args) {
     run();
+  }
+
+  public static void border(int w, int h){
+    String ws = "";
+    ws += CORNERCHAR;
+    for(int i = 0; i < (w - 2); i++){
+      ws += HORIZONTALCHAR;
+    }
+    ws += CORNERCHAR;
+    for(int i = 1; i <= h; i++){
+      if(i == 1 || i == h){
+        System.out.println(Text.colorize(ws, BACKGROUND_COLOR, BORDER_COLOR));
+      }
+      else {
+        System.out.print(Text.colorize(VERTICALCHAR, BACKGROUND_COLOR, BORDER_COLOR));
+        Text.go(i, w);
+        System.out.println(Text.colorize(VERTICALCHAR, BACKGROUND_COLOR, BORDER_COLOR));
+      }
+    }
+  }
+
+  public static String conditionalformatting(int val){
+    if(val < 15){
+      return(Text.colorize(""+val, Text.RED));
+    }
+    if(val < 25){
+      return(Text.colorize(""+val, Text.YELLOW));
+    }
+    if(val < 50){
+      return(Text.colorize(""+val, Text.WHITE));
+    }
+    return(Text.colorize(""+val, Text.GREEN));
   }
 
   //Display a List of 1-4 adventurers on the rows row through row+3 (4 rows max)
@@ -15,21 +51,45 @@ public class StuyabloGame{
     /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
     //YOUR CODE HERE
     /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+    int curcol = 2;
+    int inccol = (WIDTH - 2) / party.size();
+    for(int i = 0; i < party.size(); i++){
+      drawText(party.get(i).getName(), startRow, curcol, false);
+      drawText(""+party.get(i).getHP(), startRow+1, curcol, true);
+    }
   }
 
   //Display a line of text starting at column 2 of the specified row.
-  public static void drawText(String s,int startRow){
+  public static void drawText(String s, int startRow){
     /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
     //YOUR CODE HERE
     /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+    drawText(s, startRow, 2, false);
+  }
+
+  public static void drawText(String s, int startRow, int startCol, boolean format){
+    if(format){
+      try{
+        int val = Integer.parseInt(s);
+        s = conditionalformatting(val);
+      } catch (NumberFormatException e){
+        s = Text.colorize(s, DEFAULT_TEXT_COLOR);
+      }
+    } else {
+      s = Text.colorize(s, DEFAULT_TEXT_COLOR);
+    }
+    Text.go(startRow,startCol);
+    System.out.print(s);
   }
 
   public static void drawScreen(){
     /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
     //YOUR CODE HERE
     /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+    Text.clear();
+    Text.go(1,1);
+    border(WIDTH, HEIGHT);
   }
-
 
   public static void run(){
     //Clear and initialize
